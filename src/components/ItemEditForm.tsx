cfb111eab8163868648534e0331b32806960acf5
Erik Noorland <noorlandjr_e@hotmail.com>
2024-01-17 19:46:25 +0100

    Make the edit item form look good

--- src/components/ItemEditForm.tsx
+++ src/components/ItemEditForm.tsx
@@ -3,6 +3,7 @@ import itemTypes from '../data/itemTypes.json';
 import itemPriorities from '../data/itemPriorities.json';
 import { ItemData } from '../types';
 import './ItemEditForm.scss';
+import Button from './Button';
 
 interface ItemEditFormProps {
   data: ItemData;
@@ -36,71 +37,113 @@ const ItemEditForm = (props: ItemEditFormProps) => {
 
   return (
     <>
-    <input
-      type="text"
-      name="title"
-      className="item__titleInput"
-      defaultValue={editData.title}
-      onChange={handleOnInputChange}
-    />
-
-    <div>
-      <textarea
-        name="description"
-        className="item__descriptionInput"
-        defaultValue={editData.description}
-        onChange={handleOnInputChange}
-      />
-    </div>
-
-    <select
-      name="type"
-      defaultValue={editData.type}
-      onChange={handleOnInputChange}
-      className="item__select"
-    >
-      {itemTypes.map((type) => {
-        return <option
-          key={type.id}
-          value={type.value}
+      <div className="form__element">
+        <label
+          htmlFor="title"
+          className="form__elementLabel"
         >
-          {type.name}
-        </option>
-      })}
-    </select>
+          Title
+        </label>
 
-    <select
-      name="priority"
-      defaultValue={editData.priority}
-      onChange={handleOnInputChange}
-      className="item__select"
-    >
-      {itemPriorities.map((priority) => {
-      return <option
-        key={priority.id}
-        value={priority.value}
-      >
-        {priority.name}
-      </option>
-    })}
-    </select>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          className="form__elementInput"
+          defaultValue={editData.title}
+          onChange={handleOnInputChange}
+        />
+      </div>
 
-    <div className="item__buttons">
-      <button
-        className="item__cancelButton"
-        onClick={handleCancelClick}
-      >
-        Cancel
-      </button>
+      <div className="form__element">
+        <label
+          htmlFor="description"
+          className="form__elementLabel"
+        >
+          Description
+        </label>
+        
+        <textarea
+          id="description"
+          name="description"
+          cols={40}
+          rows={4}
+          className="form__elementTextarea"
+          defaultValue={editData.description}
+          onChange={handleOnInputChange}
+        />
+      </div>
 
-      <button
-        className="item__saveButton"
-        onClick={handleSaveClick}
-      >
-        Save
-      </button>
-    </div>
-  </>
+      <div className="form__element">
+        <label
+          htmlFor="type"
+          className="form__elementLabel"
+        >
+          Type
+        </label>
+
+        <select
+          id="type"
+          name="type"
+          defaultValue={editData.type}
+          onChange={handleOnInputChange}
+          className="form__elementSelect"
+        >
+          {itemTypes.map((type) => {
+            return <option
+              key={type.id}
+              value={type.value}
+            >
+              {type.name}
+            </option>
+          })}
+        </select>
+      </div>
+
+      <div className="form__element">
+        <label
+          htmlFor="priority"
+          className="form__elementLabel"
+        >
+          Priority
+        </label>
+
+        <select
+          id="priority"
+          name="priority"
+          defaultValue={editData.priority}
+          onChange={handleOnInputChange}
+          className="form__elementSelect"
+        >
+          {itemPriorities.map((priority) => {
+          return <option
+            key={priority.id}
+            value={priority.value}
+          >
+            {priority.name}
+          </option>
+        })}
+        </select>
+      </div>
+
+      <div className="form__element itemEditForm__buttons">
+        <Button
+          modifiers="cancel"
+          className="itemEditForm__cancelButton"
+          onClick={handleCancelClick}
+        >
+          Cancel
+        </Button>
+
+        <Button
+          modifiers="submit"
+          className="itemEditForm__saveButton"
+          onClick={handleSaveClick}
+        >
+          Save
+        </Button>
+      </div>
+    </>
   );
 };
 
